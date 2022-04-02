069cdea0470dab0cfaec8d5e50c27b8b333f27aa
Maggie Jenkins <adair.jenkins@gmail.com>
2022-04-01 22:08:37 -0500

    working on dropdown

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,12 +1,47 @@
 import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
+import Dropdown from 'react-dropdown';
 
 function Feeling() {
     const history = useHistory();
  
+    const [value, setValue] = useState('');
+    // const options = [
+    //     { label: 'Fruit', value: 'fruit' },
+    //     { label: 'Vegetable', value: 'vegetable' },
+    //     { label: 'Meat', value: 'meat' }
+    // ];
+
+    // const [value, setValue] = useState('fruit');
+
+    // const handleChange = (event) => {
+    //     setValue(event.target.value);
+    // };
+
+    const handleSelect = (event) => {
+        setValue(event.target.value);
+    }
+
+    const options1 = [
+        'one', 'two', 'three'
+      ];
+      const defaultOption = options1[0];
+    
     return (
         <>
-            <h3>FEELING COMPONENT</h3>
-            <button onClick={() => history.push('/understanding')}>NEXT</button>
+        
+        {/* <label>
+        What do we eat?
+        <select value={value} onChange={handleChange}>
+          {options.map((option) => (
+            <option value={option.value}>{option.label}</option>
+          ))}
+        </select>
+        </label>
+
+        <p>We eat {value}!</p> */}
+
+        <Dropdown options={options1} onChange={handleSelect} value={defaultOption} placeholder="Select an option" />
         </>
     )
 }
