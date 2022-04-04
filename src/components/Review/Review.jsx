3aebaadc04334f6dc02841c63992c1091a1716eb
Maggie Jenkins <adair.jenkins@gmail.com>
2022-04-03 20:07:43 -0500

    add mui table formating

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -3,6 +3,14 @@ import { useSelector } from 'react-redux';
 import axios from 'axios';
 import BackButton from '../BackButton/BackButton';
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 function Review() {
     const history = useHistory();
@@ -28,25 +36,24 @@ function Review() {
         <>
             <h3>Please review your feedback</h3>
             <BackButton handleBackBtn={handleBackBtn}/>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Feeling</th>
-                        <th>Understanding</th>
-                        <th>Support</th>
-                        <th>Comments</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{feedback.feeling}</td>
-                        <td>{feedback.understanding}</td>
-                        <td>{feedback.support}</td>
-                        <td>{feedback.comments}</td>
-                    </tr>
-                </tbody>
-            </table>
-
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Feeling</TableCell>
+                        <TableCell>Understanding</TableCell>
+                        <TableCell>Support</TableCell>
+                        <TableCell>Comments</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <TableRow>
+                        <TableCell>{feedback.feeling}</TableCell>
+                        <TableCell>{feedback.understanding}</TableCell>
+                        <TableCell>{feedback.support}</TableCell>
+                        <TableCell>{feedback.comments}</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
             <Button onClick={handleSubmission}>SUBMIT</Button>
         </>
     )
