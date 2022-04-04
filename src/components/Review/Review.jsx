29d9512d8eb6dd775a9b48ec4b0b3c9ec8cfd464
Maggie Jenkins <adair.jenkins@gmail.com>
2022-04-03 20:18:14 -0500

    planning to merge

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -3,14 +3,12 @@ import { useSelector } from 'react-redux';
 import axios from 'axios';
 import BackButton from '../BackButton/BackButton';
 import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import Rating from '@material-ui/lab/Rating';
 
 function Review() {
@@ -37,6 +35,7 @@ function Review() {
         <>
             <h3>Please review your feedback</h3>
             <BackButton handleBackBtn={handleBackBtn}/>
+            <TableContainer>
             <Table>
                 <TableHead>
                     <TableRow>
@@ -55,6 +54,7 @@ function Review() {
                     </TableRow>
                 </TableBody>
             </Table>
+            </TableContainer>
             <Button onClick={handleSubmission}>SUBMIT</Button>
         </>
     )
